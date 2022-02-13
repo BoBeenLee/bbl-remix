cdedd1ca7bfcd7804a3c83a20067519b3b84ee95
BoBeenLee <globaldev@naver.com>
2022-02-13 19:28:59 +0900

    feat: add about page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,22 @@
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+import React, { Component } from "react";
+import styled from "styled-components";
+import Layout from "~/components/Layout";
+import { Rotate } from "~/organizations/home";
+
+const Root = styled.div`
+  height: 100%;
+`;
+
+class HomePage extends Component {
+  public render() {
+    return (
+      <Layout>
+        <Root>
+          <Rotate />
+        </Root>
+      </Layout>
+    );
+  }
 }
+
+export default HomePage;
